fe3c61e42c69425de3e54a07248c904d3a6fa9d2
Denzimon <solidden@icloud.com>
2023-09-29 13:18:58 +0300

    выполнил первое дз

---
+++ src/main/java/lesson1/home.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class home {
+    public static void main(String[] args) {
+
+    }
+
+    private static void task1() {
+        System.out.println("Решение первой задачи");
+    }
+}
